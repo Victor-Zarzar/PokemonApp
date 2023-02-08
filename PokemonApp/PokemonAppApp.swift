eb9f75fa6c74f51854b52646c8df22867f26f224
Victor-Zarzar <victorzarzar58@gmail.com>
2023-02-08 14:33:08 -0300

    Initial Commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Victor Zarzar on 08/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
